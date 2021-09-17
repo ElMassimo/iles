a2cd2dc5611f7e47d5aa15fa918eb6c1c6f9e5bb
Maximo Mussini <maximomussini@gmail.com>
2021-09-17 10:16:27 -0300

    chore: fix deployment of example

--- playground/the-vue-point/src/components/Quicklink.ts
+++ playground/the-vue-point/src/components/Quicklink.ts
@@ -1,4 +1,8 @@
+// Public: Example of an island that does not bring in the Vue runtime.
+//
+// In the future <ClientScript> will be provided to make this kind of usage
+// more convenient.
 export default async function setupQuicklinks () {
-  if (import.meta.env.PROD && !import.meta.env.SSR)
+  if (!import.meta.env.SSR && import.meta.env.PROD)
     (await import('quicklink')).listen()
 }
