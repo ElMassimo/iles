e982e01820675f31feeb3c00f7a62b1516079454
Maximo Mussini <maximomussini@gmail.com>
2021-09-20 15:40:38 -0300

    fix: smaller output for islands with empty slots

--- packages/iles/src/client/app/utils.ts
+++ packages/iles/src/client/app/utils.ts
@@ -3,14 +3,14 @@ export { inBrowser } from '../shared'
 export { default as serialize } from '@nuxt/devalue'
 
 export function mapObject<I, O> (obj: Record<string, I>, fn: (i: I, key?: string) => O): Record<string, O> {
-  const result = Object.create(null)
+  const result: Record<string, O> = {}
   for (let key in obj)
     result[key] = fn(obj[key], key)
   return result
 }
 
 export async function asyncMapObject<I, O> (obj: Record<string, I>, fn: (i: I) => Promise<O>): Promise<Record<string, O>> {
-  const result = Object.create(null)
+  const result: Record<string, O> = {}
   for (let key in obj)
     result[key] = await fn(obj[key])
   return result
