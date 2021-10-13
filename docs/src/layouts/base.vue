7ca0f4baf23aa5a3902b0f878699bb647c37a991
Maximo Mussini <maximomussini@gmail.com>
2021-10-13 20:00:03 -0300

    docs: blog link fix

--- docs/src/layouts/base.vue
+++ docs/src/layouts/base.vue
@@ -2,7 +2,7 @@
 import '~/logic/quicklink'
 
 if (import.meta.env.PROD)
-  console.log('Powered by îles 🏝', 'https://iles-docs/netlify.app')
+  console.log('Powered by îles 🏝', 'https://iles-docs.netlify.app')
 </script>
 
 <template>
