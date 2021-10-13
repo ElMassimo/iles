92fb0b861ef0d22a05116800af1709d4c4517e31
Maximo Mussini <maximomussini@gmail.com>
2021-10-13 19:40:28 -0300

    deps: upgrade vite to 2.6.5

--- packages/iles/src/node/cli.ts
+++ packages/iles/src/node/cli.ts
@@ -1,13 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import { red, cyan } from 'nanocolors'
+import { red, cyan, yellow, green } from 'nanocolors'
 import minimist from 'minimist'
 import { build } from './build/build'
 import { createServer } from './server'
 
 const argv: any = minimist(process.argv.slice(2))
-
-console.info(cyan(`iles v${require('../../package.json').version}`))
-console.info(cyan(`vite v${require('vite/package.json').version}`))
 
 const command = argv._[0]
 const root = argv._[command ? 1 : 0]
@@ -15,13 +12,22 @@ if (root) argv.root = root
 
 if (!command || command === 'dev' || command === 'serve') {
   createServer(root, argv)
-    .then(({ server }) => server.listen())
+    .then(async ({ server }) => {
+      await server.listen()
+      const { config: { logger } } = server
+      logger.info(cyan(`iles v${require('../../package.json').version}`)
+        + yellow(` vite v${require('vite/package.json').version}`)
+        + green(' dev server running at:\n'), { clear: !logger.hasWarned })
+      server.printUrls()
+    })
     .catch((err: any) => {
       console.error(red('error starting server:\n'), err)
       process.exit(1)
     })
 }
 else if (command === 'build') {
+  console.info(cyan(`iles v${require('../../package.json').version}`)
+    + yellow(` vite v${require('vite/package.json').version}`))
   build(root).catch((err: any) => {
     console.error(red('build error:\n'), err)
     process.exit(1)
