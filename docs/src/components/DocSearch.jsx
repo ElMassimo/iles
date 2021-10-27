1840a159789c94cc89f89545f84552fef8cf1f0f
Maximo Mussini <maximomussini@gmail.com>
2021-10-27 15:03:13 -0300

    docs: link to pagination demo

--- docs/src/components/DocSearch.jsx
+++ docs/src/components/DocSearch.jsx
@@ -1,11 +1,26 @@
 import { DocSearch } from '@mussi/docsearch'
 import '~/styles/docsearch.css'
 
-const IlesDocSearch = (props) =>
+const options = import.meta.env.DEV
+  ? {
+    transformItems: (items) => {
+      return items.map((item) => {
+        const getRelativePath = (url) => {
+          const { pathname, hash } = new URL(url)
+          return pathname + hash
+        }
+        return Object.assign({}, item, { url: getRelativePath(item.url) })
+      })
+    },
+  }
+  : {}
+
+const IlesDocSearch = props =>
   <DocSearch
     appId="GERZE019PN"
     apiKey="cdb4a3df8ecf73fadf6bde873fc1b0d2"
     indexName="iles"
+    { ...options }
     { ...props }
   />
 
