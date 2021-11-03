821d6cf4b93d0c676fd29c0b627deca5a697a241
Maximo Mussini <maximomussini@gmail.com>
2021-11-03 12:34:09 -0300

    fix: explicitly catch any import errors that may occur

--- packages/frontmatter/src/frontmatter.cjs
+++ packages/frontmatter/src/frontmatter.cjs
@@ -1,6 +1,5 @@
 module.exports = (...args) => new Promise((resolve, reject) => {
   import('../dist/frontmatter.js')
-    .then(m => {
-      resolve(m.default(...args))
-    })
+    .then(m => resolve(m.default(...args)))
+    .catch(reject)
 })
