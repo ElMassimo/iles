60007d4f582f63ddbd356f642321ae219740770f
Maximo Mussini <maximomussini@gmail.com>
2021-11-26 12:23:55 -0300

    docs: add deployment instructions for Cloudflare Pages

---
+++ docs/src/components/CloudflareLogo.vue
@@ -0,0 +1,5 @@
+<template>
+  <a href="https://pages.cloudflare.com/" target="_blank" rel="noreferrer">
+    <IconAppCloudflare v-bind="$attrs" class="inline-block mx-1 -mt-1"/>
+  </a>
+</template>
