334a4e67bff30c624434463a95abf0692c00dc8a
Maximo Mussini <maximomussini@gmail.com>
2021-12-28 20:24:20 -0300

    fix: ensure sitemap preserves double slashes when targeting a subdomain

--- packages/iles/src/node/build/sitemap.ts
+++ packages/iles/src/node/build/sitemap.ts
@@ -30,7 +30,7 @@ function sitemapFor (siteUrl: string, routesToRender: RouteToRender[]) {
 
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${sortedUrls.map(url => `  <url><loc>${join(siteUrl, url)}</loc></url>\n`).join('')}
+${sortedUrls.map(url => `  <url><loc>${siteUrl + url.slice(1)}</loc></url>\n`).join('')}
 </urlset>
 `
 }
