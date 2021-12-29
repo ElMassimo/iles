a128ae809c085ed47261f9817fbfab2b6fa0b8a0
Maximo Mussini <maximomussini@gmail.com>
2021-12-29 18:04:44 -0300

    docs: clean up last updated at

--- docs/src/components/TimeAgo.vue
+++ docs/src/components/TimeAgo.vue
@@ -9,15 +9,14 @@ let dateStr = $computed(() => date.toLocaleDateString('en-US', { day: 'numeric',
 let timeStr = $computed(() => date.toISOString())
 
 if (!import.meta.env.SSR) {
-  const currentTime = (hoursAgo: number) => {
+  const currentTime = (hoursAgo: number): string => {
     if (hoursAgo < 1) return 'a few minutes ago'
     if (hoursAgo < 2) return 'an hour ago'
     if (hoursAgo < 8) return 'a few hours ago'
     if (hoursAgo < 24) return 'today'
-    if (hoursAgo < 24 * 30) return `${Math.ceil(hoursAgo / 24)} days ago`
-    if (hoursAgo < 24 * 30 * 2) return '1 month ago'
-    if (hoursAgo < 24 * 30 * 12) return `${Math.ceil(hoursAgo / (24 * 30))} months ago`
-    return dateStr
+    if (hoursAgo < 24 * 8) return 'this week'
+    if (hoursAgo < 24 * 30) return 'this month'
+    return date.toLocaleDateString('en-US', { day: 'numeric', month: 'long' })
   }
 
   const updateRelativeTimeStr = () =>
