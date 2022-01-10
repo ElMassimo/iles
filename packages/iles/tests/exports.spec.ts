4618f4be7118c06324d74d87d35ba73c0767369b
Maximo Mussini <maximomussini@gmail.com>
2022-01-10 13:07:19 -0300

    test: upgrade vitest to prevent timeouts

--- packages/iles/tests/exports.spec.ts
+++ packages/iles/tests/exports.spec.ts
@@ -3,7 +3,7 @@ import { test, describe, expect } from 'vitest'
 import Island from '@components/Island.vue'
 
 describe('exports', () => {
-  test('ensure island component can be resolved', async () => {
+  test('ensure island component can be resolved', () => {
     expect(Island.name).toEqual('Island')
   })
 })
