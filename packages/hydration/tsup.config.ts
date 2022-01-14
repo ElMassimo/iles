8d878cd032e4a6baa31b5ce24f9d11e0523a84c5
Maximo Mussini <maximomussini@gmail.com>
2022-01-14 17:25:51 -0300

    chore: use tsup clean instead of rimraf

--- packages/hydration/tsup.config.ts
+++ packages/hydration/tsup.config.ts
@@ -1,5 +1,6 @@
 import type { Options } from 'tsup'
 export const tsup: Options = {
+  clean: true,
   dts: true,
   target: 'es2020',
   splitting: true,
--- packages/icons/tsup.config.ts
+++ packages/icons/tsup.config.ts
@@ -1,5 +1,6 @@
 import type { Options } from 'tsup'
 export const tsup: Options = {
+  clean: true,
   dts: true,
   target: 'node14',
   splitting: true,
