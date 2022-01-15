0ce96424d822dc5f59d94ba3ca5dbb13ef3c90f6
Maximo Mussini <maximomussini@gmail.com>
2022-01-15 17:52:36 -0300

    test: combine pageData and routes test to ensure pages habe been added
    Since tracking pages is now lazily performed, page meta such as `href`
    won't be available until the routes have been imported.

--- packages/iles/tests/routes.spec.ts
+++ packages/iles/tests/routes.spec.ts
@@ -2,6 +2,9 @@ import { test, describe, expect } from 'vitest'
 
 import routes from '@islands/routes'
 
+import vue32 from '@pages/posts/vue-3-2.mdx'
+import notFound from '@pages/404.vue'
+
 describe('app routes', () => {
   test('empty', async () => {
     expect(routes.map(route => route.path)).toEqual([
@@ -14,5 +17,55 @@ describe('app routes', () => {
       '/posts/:page',
       '/:zzz(.*)*',
     ])
+  })
+})
+
+describe('page data', () => {
+  test('for markdown file', async () => {
+    const doc = vue32
+
+    expect(typeof doc.render).toEqual('function')
+    expect(typeof doc.__hmrId).toEqual('string')
+
+    const { layoutName } = doc
+    expect(layoutName).toEqual('default')
+
+    const { filename, href } = doc
+    expect(filename).toEqual('playground/the-vue-point/src/pages/posts/vue-3-2.mdx')
+    expect(href).toEqual('/posts/vue-3-2')
+
+    const { title, date, author, gravatar, twitter } = doc
+    expect(title).toEqual('Vue 3.2 Released!')
+    expect(date.constructor.toString()).toContain('Date')
+    expect(author).toEqual('Evan You')
+    expect(gravatar).toEqual('eca93da2c67aadafe35d477aa8f454b8')
+    expect(twitter).toEqual('@youyuxi')
+
+    const { frontmatter, meta, route } = doc
+    expect(frontmatter).toMatchObject({ title, author, gravatar, twitter })
+    expect(meta).toMatchObject({ filename, href })
+    expect(route).toEqual(undefined)
+  })
+
+  test('for vue file', async () => {
+    const doc = notFound
+
+    expect(typeof doc.render).toEqual('function')
+    expect(typeof doc.__hmrId).toEqual('string')
+
+    const { layoutName } = doc
+    expect(layoutName).toEqual('default')
+
+    const { filename, href } = doc
+    expect(filename).toEqual('playground/the-vue-point/src/pages/404.vue')
+    expect(href).toEqual('/404')
+
+    const { title } = doc
+    expect(title).toEqual('Not Found')
+
+    const { frontmatter, meta, route } = doc
+    expect(frontmatter).toMatchObject({ title })
+    expect(meta).toMatchObject({ filename, href })
+    expect(route).toEqual(undefined)
   })
 })
