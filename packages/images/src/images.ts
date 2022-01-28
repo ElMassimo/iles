da37207abb4dd4d37bbb561362facf30d5e847e6
Maximo Mussini <maximomussini@gmail.com>
2022-01-28 17:38:18 -0300

    fix: windows compatibility for the Picture resolution

--- packages/images/src/images.ts
+++ packages/images/src/images.ts
@@ -1,4 +1,4 @@
-import { dirname, resolve } from 'path'
+import { dirname, resolve } from 'pathe'
 import { fileURLToPath } from 'url'
 import type { IlesModule } from 'iles'
 import type { ImageApi, ImagePresets, Options } from 'vite-plugin-image-presets'
