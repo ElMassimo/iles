c910492336da1e6c77cc126d77a6bc9e4c626d9e
Maximo Mussini <maximomussini@gmail.com>
2022-02-14 12:35:57 -0300

    fix: circumvent optimization when hydrating single nodes by using render

--- packages/hydration/solid.ts
+++ packages/hydration/solid.ts
@@ -1,14 +1,20 @@
-import { hydrate, createComponent } from 'solid-js/web'
-import type { Component } from 'solid-js'
+import { hydrate, render, createComponent } from 'solid-js/web'
+import type { Component, JSX } from 'solid-js'
 import type { Props, Slots } from './types'
 import { onDispose } from './hydration'
 
+let renderComponent = hydrate
+
+if (import.meta.env.DEV)
+  renderComponent = (component, el, options) =>
+    el.childNodes.length ? hydrate(component, el, options) : render(component, el)
+    
 export default function createIsland (component: Component, id: string, el: Element, props: Props, slots: Slots | undefined) {
   if (import.meta.env.DEV)
     // @ts-ignore
     window._$HY ||= { events: [], completed: new WeakSet(), r: {} }
 
-  const dispose = hydrate(() => createComponent(component, { ...props, children: createContent(slots) }), el, { renderId: id })
+  const dispose = renderComponent(() => createComponent(component, { ...props, children: createContent(slots) }), el, { renderId: id })
 
   if (import.meta.env.DISPOSE_ISLANDS)
     onDispose(id, dispose)
