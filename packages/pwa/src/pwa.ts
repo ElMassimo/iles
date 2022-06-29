25de4acc2a85309c3fcbbce9a78d7bd40ef741df
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2022-06-29 22:04:38 +0200

    feat(pwa): use `ManifestTransform` when using `@islands/pwa`

--- packages/pwa/src/pwa.ts
+++ packages/pwa/src/pwa.ts
@@ -2,67 +2,26 @@ import fs from 'fs'
 import crypto from 'crypto'
 import { resolve } from 'path'
 import { performance } from 'perf_hooks'
-import type { IlesModule, UserConfig } from 'iles'
+import type { IlesModule, RouteToRender, UserConfig } from 'iles'
 import type { VitePluginPWAAPI, VitePWAOptions } from 'vite-plugin-pwa'
-import type { ManifestEntry } from 'workbox-build'
+import type { ManifestEntry, ManifestTransform } from 'workbox-build'
 import { VitePWA } from 'vite-plugin-pwa'
+
+interface ManifestTransformData {
+  outDir: string
+  pages: RouteToRender[]
+}
+
+interface EnableManifestTransformData {
+  enable: boolean
+  data?: ManifestTransformData
+}
+
+type EnableManifestTransform = () => EnableManifestTransformData
 
 function timeSince (start: number): string {
   const diff = performance.now() - start
   return diff < 750 ? `${Math.round(diff)}ms` : `${(diff / 1000).toFixed(1)}s`
-}
-
-function configureDefaults (config: UserConfig, options: Partial<VitePWAOptions> = {}): Partial<VitePWAOptions> {
-  const {
-    strategies = 'generateSW',
-    registerType = 'prompt',
-    injectRegister,
-    workbox = {},
-    injectManifest = {},
-    ...rest
-  } = options
-
-  if (strategies === 'generateSW') {
-    const useWorkbox = { ...workbox }
-    const newOptions: Partial<VitePWAOptions> = {
-      ...rest,
-      strategies,
-      registerType,
-      injectRegister,
-    }
-    // we use route names: use Vite base or its default
-    if (!useWorkbox.navigateFallback || useWorkbox.navigateFallback.endsWith('.html'))
-      useWorkbox.navigateFallback = config.vite?.base ?? '/'
-
-    if (registerType === 'autoUpdate') {
-      if (useWorkbox.clientsClaim === undefined)
-        useWorkbox.clientsClaim = true
-
-      if (useWorkbox.skipWaiting === undefined)
-        useWorkbox.skipWaiting = true
-    }
-
-    // we don't need registerSW.js if not configured
-    if (injectRegister === undefined)
-      newOptions.injectRegister = null
-
-    newOptions.workbox = useWorkbox
-
-    return newOptions
-  }
-
-  // we don't need registerSW.js if not configured
-  if (injectRegister === undefined) {
-    return {
-      ...rest,
-      strategies,
-      registerType,
-      injectManifest,
-      injectRegister: null,
-    }
-  }
-
-  return options
 }
 
 function buildManifestEntry (
@@ -87,6 +46,110 @@ function buildManifestEntry (
   })
 }
 
+function buildManifestEntryTransform (
+  url: string,
+  path: string,
+): Promise<ManifestEntry & { size: number }> {
+  return buildManifestEntry(url, path).then(({ url, revision }) => {
+    return new Promise((resolve, reject) => {
+      try {
+        const { size } = fs.statSync(path)
+        return resolve({ url, revision, size })
+      }
+      catch (e) {
+        reject(e)
+      }
+    })
+  })
+}
+
+function createManifestTransform (enableManifestTransform: EnableManifestTransform): ManifestTransform {
+  return async (entries) => {
+    const { enable, data } = enableManifestTransform()
+    console.log(`Calling ManifestTransform: ${enable}`)
+    if (enable && data) {
+      const { outDir, pages } = data
+      console.log(entries.filter(e => e.url.endsWith('.html')))
+      const manifest = entries.filter(e => !e.url.endsWith('.html'))
+      const addRoutes = await Promise.all(pages.map((r) => {
+        return buildManifestEntryTransform(r.path, resolve(outDir, r.outputFilename))
+      }))
+      console.log(addRoutes)
+      manifest.push(...addRoutes)
+      return { manifest }
+    }
+
+    return { manifest: entries }
+  }
+}
+
+function configureDefaults (
+  config: UserConfig,
+  enableManifestTransform: EnableManifestTransform,
+  options: Partial<VitePWAOptions> = {},
+): Partial<VitePWAOptions> {
+  const {
+    strategies = 'generateSW',
+    registerType = 'prompt',
+    injectRegister,
+    workbox = {},
+    injectManifest = {},
+    ...rest
+  } = options
+
+  if (strategies === 'generateSW') {
+    const useWorkbox = { ...workbox }
+    const newOptions: Partial<VitePWAOptions> = {
+      ...rest,
+      strategies,
+      registerType,
+      injectRegister,
+    }
+    const prettyUrls = config.prettyUrls ?? true
+    if (!useWorkbox.navigateFallback && prettyUrls)
+      useWorkbox.navigateFallback = config.vite?.base ?? '/'
+
+    // TODO: remove this when https://github.com/antfu/vite-plugin-pwa/pull/306 released
+    if (registerType === 'autoUpdate') {
+      if (useWorkbox.clientsClaim === undefined)
+        useWorkbox.clientsClaim = true
+
+      if (useWorkbox.skipWaiting === undefined)
+        useWorkbox.skipWaiting = true
+    }
+
+    // we don't need registerSW.js if not configured
+    if (injectRegister === undefined)
+      newOptions.injectRegister = null
+
+    newOptions.workbox = useWorkbox
+
+    newOptions.workbox.manifestTransforms = newOptions.workbox.manifestTransforms ?? []
+    newOptions.workbox.manifestTransforms.push(createManifestTransform(enableManifestTransform))
+
+    return newOptions
+  }
+
+  // we don't need registerSW.js if not configured
+  if (injectRegister === undefined) {
+    injectManifest.manifestTransforms = injectManifest.manifestTransforms ?? []
+    injectManifest.manifestTransforms.push(createManifestTransform(enableManifestTransform))
+    return {
+      ...rest,
+      strategies,
+      registerType,
+      injectManifest,
+      injectRegister: null,
+    }
+  }
+
+  options.injectManifest = options.injectManifest ?? {}
+  options.injectManifest.manifestTransforms = injectManifest.manifestTransforms ?? []
+  options.injectManifest.manifestTransforms.push(createManifestTransform(enableManifestTransform))
+
+  return options
+}
+
 /**
  * An iles module that configures vite-plugin-pwa and
  * regenerates the PWA when SSG is finished.
@@ -95,6 +158,9 @@ function buildManifestEntry (
  */
 export default function IlesPWA (options: Partial<VitePWAOptions> = {}): IlesModule {
   let api: VitePluginPWAAPI | undefined
+  let enable = false
+  let data: ManifestTransformData | undefined
+  let enableManifestTransform: EnableManifestTransform | undefined
   return {
     name: '@islands/pwa',
     config (config) {
@@ -103,7 +169,10 @@ export default function IlesPWA (options: Partial<VitePWAOptions> = {}): IlesMod
         api = plugin.api
       }
       else {
-        const pluginPWA = VitePWA(configureDefaults(config, options))
+        enableManifestTransform = () => {
+          return { enable, data }
+        }
+        const pluginPWA = VitePWA(configureDefaults(config, enableManifestTransform, options))
         api = pluginPWA.find(p => p.name === 'vite-plugin-pwa')?.api
         return {
           vite: {
@@ -117,14 +186,21 @@ export default function IlesPWA (options: Partial<VitePWAOptions> = {}): IlesMod
         if (api && !api.disabled) {
           console.info('Regenerating PWA service worker...')
           const startTime = performance.now()
-          const addRoutes = await Promise.all(pages.map((r) => {
-            return buildManifestEntry(r.path, resolve(outDir, r.outputFilename))
-          }))
-          api.extendManifestEntries((manifestEntries) => {
-            // just add the routes: the returned value will override existing entry
-            manifestEntries.push(...addRoutes)
-            return undefined
-          })
+          if (enableManifestTransform) {
+            enable = true
+            data = { outDir, pages }
+          }
+          else {
+            // TODO: we can do this only if prettyURL is enabled: the downside is the html and the logic route added twice (/about and about.html)
+            const addRoutes = await Promise.all(pages.map((r) => {
+              return buildManifestEntry(r.path, resolve(outDir, r.outputFilename))
+            }))
+            api.extendManifestEntries((manifestEntries) => {
+              // just add the routes: the returned value will override existing entry
+              manifestEntries.push(...addRoutes)
+              return undefined
+            })
+          }
           // generate the manifest.webmanifest file
           api.generateBundle()
           // regenerate the sw
