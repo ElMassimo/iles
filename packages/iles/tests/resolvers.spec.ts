21cbbbae7d870621ea727db6efcca18c05d47ffc
Maximo Mussini <maximomussini@gmail.com>
2022-07-11 18:32:46 -0300

    chore: fix tests

--- packages/iles/tests/resolvers.spec.ts
+++ packages/iles/tests/resolvers.spec.ts
@@ -6,8 +6,8 @@ import { ISLAND_COMPONENT_PATH } from '@node/alias'
 describe('resolvers', () => {
   test('can resolve Island and Head', async () => {
     const resolve = IlesComponentResolver
-    expect(resolve('Island')).toEqual({ path: ISLAND_COMPONENT_PATH })
-    expect(resolve('Head')).toEqual({ importName: 'Head', path: '@vueuse/head' })
+    expect(resolve('Island')).toEqual({ from: ISLAND_COMPONENT_PATH })
+    expect(resolve('Head')).toEqual({ name: 'Head', from: '@vueuse/head' })
     expect(resolve('Something')).toEqual(undefined)
   })
 
@@ -15,10 +15,10 @@ describe('resolvers', () => {
     const resolve = IlesLayoutResolver({ layoutsDir: '/layouts' })
 
     expect(resolve('DefaultLayout'))
-      .toEqual({ importName: 'default', path: '/layouts/default.vue' })
+      .toEqual({ name: 'default', from: '/layouts/default.vue' })
 
     expect(resolve('SomethingElseLayout'))
-      .toEqual({ importName: 'default', path: '/layouts/somethingElse.vue' })
+      .toEqual({ name: 'default', from: '/layouts/somethingElse.vue' })
 
     expect(resolve('Layout')).toEqual(undefined)
   })
