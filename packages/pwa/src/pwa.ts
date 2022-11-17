f758c27d02fc333aa7bf34ec2b065b235c72bc13
userquin <userquin@gmail.com>
2022-11-17 18:03:03 +0100

    feat(pwa): update pwa logic

--- packages/pwa/src/pwa.ts
+++ packages/pwa/src/pwa.ts
@@ -1,75 +1,33 @@
-import fs from 'fs'
-import crypto from 'crypto'
-import { resolve } from 'path'
 import { performance } from 'perf_hooks'
-import type { IlesModule, RouteToRender, UserConfig } from 'iles'
+import type { IlesModule, UserConfig } from 'iles'
 import type { VitePluginPWAAPI, VitePWAOptions } from 'vite-plugin-pwa'
-import type { ManifestEntry, ManifestTransform } from 'workbox-build'
+import type { ManifestTransform } from 'workbox-build'
 import { VitePWA } from 'vite-plugin-pwa'
 
-interface ManifestTransformData {
-  outDir: string
-  pages: RouteToRender[]
+interface PWAContext {
+  enabled: boolean
+  prettyUrls: boolean
+  base: string
 }
 
-type EnableManifestTransform = () => ManifestTransformData
+type PWAContextResolver = () => PWAContext
 
 function timeSince (start: number): string {
   const diff = performance.now() - start
   return diff < 750 ? `${Math.round(diff)}ms` : `${(diff / 1000).toFixed(1)}s`
 }
 
-function buildManifestEntry (
-  url: string,
-  path: string,
-): Promise<ManifestEntry> {
-  return new Promise((resolve, reject) => {
-    const cHash = crypto.createHash('MD5')
-    const stream = fs.createReadStream(path)
-    stream.on('error', (err) => {
-      reject(err)
-    })
-    stream.on('data', (chunk) => {
-      cHash.update(chunk)
-    })
-    stream.on('end', () => {
-      return resolve({
-        url,
-        revision: `${cHash.digest('hex')}`,
-      })
-    })
-  })
-}
-
-async function buildManifestEntryTransform (
-  ssgUrl: string,
-  path: string,
-): Promise<ManifestEntry & { size: number }> {
-  const [size, { url, revision }] = await Promise.all([
-    new Promise<number>((resolve, reject) => {
-      fs.lstat(path, (err, stats) => {
-        if (err)
-          reject(err)
-        else
-          resolve(stats.size)
-      })
-    }),
-    buildManifestEntry(ssgUrl, path),
-  ])
-  return { url, revision, size }
-}
-
-function createManifestTransform (enableManifestTransform: EnableManifestTransform): ManifestTransform {
+function createManifestTransform (resolve: PWAContextResolver): ManifestTransform {
   return async (entries) => {
-    const data = enableManifestTransform()
-    if (data) {
-      const { outDir, pages } = data
-      const manifest = entries.filter(e => !e.url.endsWith('.html'))
-      const addRoutes = await Promise.all(pages.map((r) => {
-        return buildManifestEntryTransform(r.path, resolve(outDir, r.outputFilename))
-      }))
-      manifest.push(...addRoutes)
-      return { manifest }
+    const { base, enabled, prettyUrls } = resolve()
+    // entries already in the precache manifest, we only need to change the mapping when prettyUrls is enabled
+    if (enabled && prettyUrls) {
+      entries.filter(e => e.url.endsWith('.html')).forEach((e) => {
+        if (e.url === 'index.html')
+          e.url = base
+        else
+          e.url = e.url.replace(/\.html$/, '')
+      })
     }
 
     return { manifest: entries }
@@ -78,7 +36,7 @@ function createManifestTransform (enableManifestTransform: EnableManifestTransfo
 
 function configureDefaults (
   config: UserConfig,
-  enableManifestTransform: EnableManifestTransform,
+  resolve: PWAContextResolver,
   options: Partial<VitePWAOptions> = {},
 ): Partial<VitePWAOptions> {
   const {
@@ -98,21 +56,33 @@ function configureDefaults (
       registerType,
       injectRegister,
     }
-    const prettyUrls = config.prettyUrls ?? true
-    if (!useWorkbox.navigateFallback && prettyUrls)
-      useWorkbox.navigateFallback = config.vite?.base ?? '/'
 
     newOptions.workbox = useWorkbox
 
     newOptions.workbox.manifestTransforms = newOptions.workbox.manifestTransforms ?? []
-    newOptions.workbox.manifestTransforms.push(createManifestTransform(enableManifestTransform))
+    newOptions.workbox.manifestTransforms.push(createManifestTransform(resolve))
+
+    // configure 404 navigation fallback only if not already configured
+    if (!useWorkbox.navigateFallback) {
+      newOptions.integration = {
+        configureOptions (_, pwaOptions) {
+          // pwaOptions is the same as newOptions: just adding this, linter complains
+          pwaOptions.workbox = pwaOptions.workbox ?? {}
+          const { base, prettyUrls } = resolve()
+          if (prettyUrls)
+            pwaOptions.workbox.navigateFallback = `${base}404`
+          else
+            pwaOptions.workbox.navigateFallback = `${base}404.html`
+        },
+      }
+    }
 
     return newOptions
   }
 
   options.injectManifest = options.injectManifest ?? {}
   options.injectManifest.manifestTransforms = injectManifest.manifestTransforms ?? []
-  options.injectManifest.manifestTransforms.push(createManifestTransform(enableManifestTransform))
+  options.injectManifest.manifestTransforms.push(createManifestTransform(resolve))
 
   return options
 }
@@ -125,35 +95,40 @@ function configureDefaults (
  */
 export default function IlesPWA (options: Partial<VitePWAOptions> = {}): IlesModule {
   let api: VitePluginPWAAPI | undefined
-  let data: ManifestTransformData | undefined
-  const enableManifestTransform: EnableManifestTransform = () => {
-    return data!
+  const ctx: PWAContext = {
+    enabled: false,
+    prettyUrls: true,
+    base: '/',
+  }
+  const resolver: PWAContextResolver = () => {
+    return ctx
   }
   return {
     name: '@islands/pwa',
     config (config) {
       const plugin = config.vite?.plugins?.flat(Infinity).find(p => p.name === 'vite-plugin-pwa')
-      if (plugin) {
+      if (plugin)
         throw new Error('Remove the vite-plugin-pwa plugin from Vite plugins entry in iles config file, configure it via @islands/pwa plugin')
-      }
-      else {
-        const pluginPWA = VitePWA(configureDefaults(config, enableManifestTransform, options))
-        api = pluginPWA.find(p => p.name === 'vite-plugin-pwa')?.api
-        return {
-          vite: {
-            plugins: [pluginPWA],
-          },
-        }
+
+      const pluginPWA = VitePWA(configureDefaults(config, resolver, options))
+      api = pluginPWA.find(p => p.name === 'vite-plugin-pwa')?.api
+      return {
+        vite: {
+          plugins: [pluginPWA],
+        },
       }
     },
+    configResolved ({ base, prettyUrls }) {
+      // the hook will be called before the pwa plugin integration hook
+      ctx.base = base
+      ctx.prettyUrls = prettyUrls
+    },
     ssg: {
-      async onSiteRendered ({ pages, config: { outDir } }) {
+      async onSiteRendered () {
         if (api && !api.disabled) {
           console.info('Regenerating PWA service worker...')
           const startTime = performance.now()
-          data = { outDir, pages }
-          // generate the manifest.webmanifest file
-          api.generateBundle()
+          ctx.enabled = true
           // regenerate the sw
           await api.generateSW()
           console.info(`\n√  PWA done in ${timeSince(startTime)}\n`)
