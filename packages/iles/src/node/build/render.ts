4782b2454b37163b8e3c4839b320b57c9297ba2f
Dhruv Bhanushali <dhruv_b@live.com>
2023-01-13 12:41:13 +0400

    Add support for body tags

--- packages/iles/src/node/build/render.ts
+++ packages/iles/src/node/build/render.ts
@@ -54,7 +54,7 @@ export async function renderPage (
   if (!route.outputFilename.endsWith('.html'))
     return content
 
-  const { headTags, htmlAttrs, bodyAttrs } = renderHeadToString(head)
+  const { headTags, htmlAttrs, bodyTags, bodyAttrs } = await renderHeadToString(head)
 
   return `<!DOCTYPE html>
 <html ${htmlAttrs}>
@@ -65,6 +65,7 @@ export async function renderPage (
   </head>
   <body ${bodyAttrs}>
     <div id="app">${content}</div>
+    ${bodyTags}
   </body>
 </html>`
 }
