c0a3482924afab93081db8dc8174c0aaa6b81b9b
Maximo Mussini <maximomussini@gmail.com>
2023-03-30 10:52:11 -0300

    chore: fix typechecking for docs

--- docs/src/app.ts
+++ docs/src/app.ts
@@ -7,16 +7,19 @@ import { defineApp } from 'iles'
 import Image from '~/components/Image.vue'
 
 import checkDarkTheme from '~/logic/dark-color-scheme-check?raw'
+import type { Script } from '@unhead/schema'
+
+type TurboScript = Script & { once: true }
 
 const prodScripts = import.meta.env.PROD ? [
-  { src: 'https://unpkg.com/thesemetrics@latest', async: true, once: true, crossorigin: 'anonymous' },
+  { src: 'https://unpkg.com/thesemetrics@latest', async: true, once: true, crossorigin: 'anonymous' } as TurboScript,
 ] : []
 
 export default defineApp({
   head: {
     htmlAttrs: { lang: 'en-US' },
     script: [
-      { children: checkDarkTheme, once: true },
+      { children: checkDarkTheme, once: true } as TurboScript,
       ...prodScripts,
     ],
   },
