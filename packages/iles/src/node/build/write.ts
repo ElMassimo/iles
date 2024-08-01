71d48d534b5c874ebad2ae11aa052203859392dc
Akayy <techakayy@gmail.com>
2024-08-01 22:04:16 +1000

    vite 5 breaking change - manifest within .vite subfolder within outDir

--- packages/iles/src/node/build/write.ts
+++ packages/iles/src/node/build/write.ts
@@ -91,7 +91,7 @@ function resolveManifestEntries(manifest: Manifest, entryNames: string[]): strin
 }
 
 async function parseManifest(outDir: string, islandsByPath: IslandsByPath) {
-  const manifestPath = join(outDir, 'manifest.json')
+  const manifestPath = join(outDir, '.vite', 'manifest.json')
   try {
     return JSON.parse(await fs.readFile(manifestPath, 'utf-8'))
   }
